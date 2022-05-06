48615331a855610c7c6154267e6c3d8117385521
gh4sstly <matej.zahalka@seznam.cz>
2022-05-06 13:38:37 +0200

    Commit :)

--- main.js
+++ main.js
@@ -4,11 +4,10 @@ var logging = new Logging();
 var game = document.getElementById("game");
 var ball = document.getElementById("ball");
 var clickableArea = document.getElementById("clickable-area");
-
 var gameWidth;
 var gameHeight;
 var position;
-var ballRadius = 100;
+var ballRadius = 75;
 
 var xVelocity;
 var yVelocity;
@@ -24,13 +23,25 @@ function setupGame() {
     top: this.gameHeight / 2,
   };
 
+  this.ball.style.left = `${position.left - ballRadius}px`;
+  this.ball.style.top = `${position.top - ballRadius}px`;
+
   xVelocity = 0;
   yVelocity = 0;
   gravity = 0;
-
   logging.debug(`Game width: ${this.gameWidth}`);
   logging.debug(`Game height: ${this.gameHeight}`);
 }
+
+/* movement interval */
+setInterval(() => {
+  this.moveBall(this.xVelocity, this.yVelocity);
+}, 13);
+
+/* collision check interval */
+setInterval(() => {
+  this.checkForCollision();
+}, 13);
 
 /* event listeners */
 window.addEventListener("resize", function () {
@@ -39,73 +50,60 @@ window.addEventListener("resize", function () {
 
 clickableArea.addEventListener("click", (event) => {
   logging.debug(`Mouse click: {x: ${event.layerX}, y: ${event.layerY}}`);
-  console.log(event);
+
   var xDiff = event.layerX - this.position.left;
   var yDiff = event.layerY - this.position.top;
 
-  console.log(xDiff);
-  console.log(yDiff);
   if (this.isInsideCircle(event)) {
-    this.resetVelocity();
-
     if (xDiff > 0) {
-      this.xVelocity = -xDiff * 0.2 - 2;
+      this.xVelocity = -xDiff * 0.4;
     }
     if (xDiff < 0) {
-      this.xVelocity = -xDiff * 0.2 - 2;
+      this.xVelocity = -xDiff * 0.4;
     }
     if (yDiff > 0) {
-      this.yVelocity = -yDiff * 0.2 - 2;
+      this.yVelocity = -yDiff * 0.2 - 8;
     }
     if (yDiff < 0) {
-      this.yVelocity = -yDiff * 0.2 - 2;
+      this.yVelocity = -yDiff * 0.2 - 8;
     }
   }
 });
 
-this.ball.style.left = `${position.left - ballRadius}px`;
-this.ball.style.top = `${position.top - ballRadius}px`;
-
-// movement interval
-setInterval(() => {
-  this.moveBall(this.xVelocity, this.yVelocity);
-}, 13);
-
-// collision check interval
-setInterval(() => {
-  this.checkForCollision();
-}, 13);
-
 function moveBall(xVelocity, yVelocity) {
   this.position.left = this.position.left + xVelocity;
   this.position.top = this.position.top + yVelocity;
+
+  /* gravity multiplier */
+  if (this.gravity < 18) {
+    this.gravity = gravity * 1.05;
+    this.position.top = this.position.top + this.gravity;
+  }
 
   this.ball.style.top = `${this.position.top - ballRadius}px`;
   this.ball.style.left = `${this.position.left - ballRadius}px`;
 }
 
 function checkForCollision() {
-  if (this.position.left < 0 + ballRadius) {
-    this.position.left = 0 + ballRadius;
+  /* walls */
+  if (this.position.left < 0 + this.ballRadius) {
+    this.position.left = 0 + this.ballRadius;
     this.xVelocity = -this.xVelocity;
   }
-  if (this.position.left > window.innerWidth - ballRadius) {
-    this.position.left = window.innerWidth - ballRadius;
+  if (this.position.left > this.gameWidth - this.ballRadius) {
+    this.position.left = this.gameWidth - this.ballRadius;
     this.xVelocity = -this.xVelocity;
   }
-  if (this.position.top < 0 + ballRadius) {
-    this.position.top = 0 + ballRadius;
-    this.yVelocity = -this.yVelocity;
-  }
-  if (this.position.top > window.innerHeight - ballRadius) {
-    this.position.top = window.innerHeight - ballRadius;
-    this.yVelocity = -this.yVelocity;
-  }
-}
 
-function resetVelocity() {
-  this.xVelocity = 0;
-  this.yVelocity = 0;
+  /* roof */
+  if (this.position.top < 0 + this.ballRadius) {
+    this.position.top = 0 + this.ballRadius;
+    this.yVelocity = -this.yVelocity;
+  }
+
+  /* floor */
+  if (this.position.top > this.gameHeight - this.ballRadius) {
+  }
 }
 
 function isInsideCircle(event) {
